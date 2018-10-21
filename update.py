bae2cb099a964a2aa6dd2ec7913de13b36de9421
lionel <lionel.bertaux@gmail.com>
2018-10-21 16:25:13 +0200

    Update now pushing to real files after saving original file with date and time. Add mode handling. Modes available: update and clean

--- update.py
+++ update.py
@@ -1,18 +1,31 @@
+## Author: Lionel Bertaux
+## Date: 21-10-2018
 ## Python script to udpate a linux user profile with custom preferences
-# Updates the files contained in the JSON in file-list. Required fiels:
-# file: name of the file in the folder
-# path: path to replace with its path + name
-
 
 from os.path import expanduser
+from shutil import copyfile
+
+import datetime
 import json
 import os
+import argparse
+
 
 # Static configuration
 fileJSON = "file-list"
 
 
+def saveFile(fPath):
+    if os.path.exists(fPath):
+        # generate a copy of the current config file
+        today = str(datetime.datetime.now()).split(".")[0].replace(":","").replace("-","").replace(" ", "-")
+        copyfile(fPath, fPath+str("_")+str(today))
+
+
 def readBashAliases(fPath):
+    # Read bash_aliases file and return the content in a dict
+    # Lines starting with a '##' are interpreted as a topic
+    # Comments ('#') are copied as well
     with open(fPath) as f:
         lines = f.readlines()
     lines = [x.strip() for x in lines]
@@ -38,49 +51,84 @@ def readBashAliases(fPath):
     return lines_dict
 
 
+def updateFiles(homeDir, fileList):
+    # update files contained in list
+    # Smart update for bash_aliases
+    # Simple copy for others
+    for cFile in fileList:
+        print("Updating " + cFile['path'] + " with content of " + cFile["file"])
+        # read current configuration file
+        fullPath = homeDir + os.sep + cFile['path']
+        # bash aliases, try to decode the file and stire in dict
+        if cFile['file'] == "bash_aliases":
+            new_file = readBashAliases(fPath=cFile['file'])
+            if os.path.exists(fullPath):
+                current_file = readBashAliases(fPath=fullPath)
+            else:
+                current_file = {}
+            updated_file = {}
+            [ updated_file.update({k: v}) for k,v in current_file.items()]
+            # Now compare both files
+            for topic,value in new_file.items():
+                # search for the same alias key
+                for short, cmd in value.items():
+                    matches = [ k for val in current_file.values() for k,v in val.items() if short == k]
+                    # for val in current_file.values():
+                    #     for k,v in val.items():
+                    #         print(str(k) + ": " +str(v) )
+                    if len(matches) == 1:
+                        continue
+                    else:
+                        # Add the values to the updated file
+                        if topic in updated_file.keys():
+                            updated_file[topic].update({short: cmd})
+                        else:
+                            updated_file.update({topic: {short: cmd}})
+            # Save current file
+            saveFile(fullPath)
+            # Now print the new file
+            f = open(fullPath, "w")
+            for topic,value in updated_file.items():
+                # search for the same alias key
+                f.write("\n##"+topic + "\n")
+                for short, cmd in value.items():
+                    # f.write("alias " + str(short) + ":" + str(cmd) + "\n")
+                    f.write(str(cmd)+"\n")
+            f.close()
+        else:
+            # for other files, simply save the current config and copy the new one (no smart update)
+            saveFile(fullPath)
+            copyfile(cFile['file'], fullPath)
+
+
+def cleanFiles(homeDir, fileList):
+    for cFile in fileList:
+        print("Cleaning " + cFile['path'] + "_datetime files.")
+        # List similar files in the repertory
+        fullPath = homeDir + os.sep + cFile['path']
+        path_split = os.path.split(fullPath)
+        files = os.listdir(path_split[0])
+        for f in files:
+            if path_split[1] in f and len(f) > len(path_split[1]):
+                print("Removing file " + os.path.join(path_split[0], f))
+                os.remove(os.path.join(path_split[0], f))
+
+
+parser = argparse.ArgumentParser(description="Process cli arguments.")
+parser.add_argument("--mode", dest="mode", action="store")
+args = parser.parse_args()
+
 # Configuration / Variable collection
 homeDir = expanduser("~")
 with open(fileJSON) as json_data:
     fileList = json.load(json_data)
-
 print("Home directory of user is: " + homeDir)
-for cFile in fileList:
-    print("Updating " + cFile['path'] + " with content of " + cFile["file"])
-    # read current configuration file
-    fullPath = homeDir + os.sep + cFile['path']
-    # bash aliases, try to decode the file and stire in dict
-    if cFile['file'] == "bash_aliases":
-        # print("---------------------")
-        current_file = readBashAliases(fPath=fullPath)
-        # print(current_file)
-        # print("---------------------")
-        new_file = readBashAliases(fPath=cFile['file'])
-        # print(new_file)
-        updated_file = {}
-        [ updated_file.update({k: v}) for k,v in current_file.items()]
-        # Now compare both files
-        for topic,value in new_file.items():
-            # search for the same alias key
-            for short, cmd in value.items():
-                matches = [ k for val in current_file.values() for k,v in val.items() if short == k]
-                # for val in current_file.values():
-                #     for k,v in val.items():
-                #         print(str(k) + ": " +str(v) )
-                if len(matches) == 1:
-                    continue
-                else:
-                    # Add the values to the updated file
-                    if topic in updated_file.keys():
-                        updated_file[topic].update({short: cmd})
-                    else:
-                        updated_file.update({topic: {short: cmd}})
-        # print(updated_file)
-        # Now print the new file
-        f = open(fullPath+str("tmp"), "w")
-        for topic,value in updated_file.items():
-            # search for the same alias key
-            f.write("\n##"+topic + "\n")
-            for short, cmd in value.items():
-                # f.write("alias " + str(short) + ":" + str(cmd) + "\n")
-                f.write(str(cmd)+"\n")
-        f.close()
+if args.mode:
+    if args.mode == "update":
+        updateFiles(homeDir=homeDir, fileList=fileList)
+    elif args.mode == "clean":
+        cleanFiles(homeDir=homeDir, fileList=fileList)
+    else:
+        print("Invalid mode. Available: 'update'.")
+
+
